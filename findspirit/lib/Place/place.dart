22b595b6020ec13784cd9fcc4e3f99c904dec0cd
YimKeul <leesjun29@gmail.com>
2022-12-27 11:28:35 +0900

    feat : edit appbar

--- findspirit/lib/Place/place.dart
+++ findspirit/lib/Place/place.dart
@@ -9,24 +9,34 @@ class Place extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 121, 69, 69),
+        backgroundColor: Colors.white,
         foregroundColor: Colors.black,
-        // leading: Icon(
-        //   Icons.menu,
-        //   size: 24,
-        // ),
+        iconTheme: IconThemeData(size: 40),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: [
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: 21),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/cart');
-                },
-                icon: Icon(
-                  Icons.shopping_cart,
-                  size: 24,
-                ),
-              )),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/');
+            },
+            icon: Icon(
+              Icons.home,
+              // size: 40,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/cart');
+            },
+            icon: Icon(
+              Icons.shopping_cart,
+              // size: 40,
+            ),
+          )
         ],
       ),
       body: MapSample(),
