3e0908b0d0633ccd9b52a3118f1eec0ce6a5839e
Ham3798 <43927226+Ham3798@users.noreply.github.com>
2022-12-27 16:02:48 +0900

    feat : googleMapSample.dart getCurrentPosition

--- findspirit/lib/Place/Widgets/googleMapSample.dart
+++ findspirit/lib/Place/Widgets/googleMapSample.dart
@@ -1,51 +1,76 @@
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapSample extends StatefulWidget {
-  late final String title;
-
+  const MapSample({Key? key}) : super(key: key);
   @override
-  _MapSampleState createState() => _MapSampleState();
+  State<MapSample> createState() => _UserMapInfoState();
 }
 
-class _MapSampleState extends State<MapSample> {
-  // 애플리케이션에서 지도를 이동하기 위한 컨트롤러
-  late GoogleMapController _controller;
-
-  // 이 값은 지도가 시작될 때 첫 번째 위치입니다.
-  final CameraPosition _initialPosition =
-      CameraPosition(target: LatLng(41.017901, 28.847953));
-
+class _UserMapInfoState extends State<MapSample> {
   // 지도 클릭 시 표시할 장소에 대한 마커 목록
   final List<Marker> markers = [];
 
+  late GoogleMapController mapController;
+  late LatLng _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  // cur location fetch
+  getLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double lat = position.latitude;
+    double long = position.longitude;
+
+    LatLng location = LatLng(lat, long);
+
+    setState(() {
+      _currentPosition = location;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   addMarker(cordinate) {
     int id = Random().nextInt(100);
-
     setState(() {
       markers
           .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+      print(markers);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: _initialPosition,
-      mapType: MapType.normal,
-      onMapCreated: (controller) {
-        setState(() {
-          _controller = controller;
-        });
-      },
-      markers: markers.toSet(),
-
-      // 클릭한 위치가 중앙에 표시
-      onTap: (cordinate) {
-        _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
-        addMarker(cordinate);
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _currentPosition,
+          zoom: 16.0,
+        ),
+        markers: markers.toSet(),
+        onTap: (cordinate) {
+          mapController.animateCamera(CameraUpdate.newLatLng(cordinate));
+          addMarker(cordinate);
+        },
+      ),
     );
   }
 }
