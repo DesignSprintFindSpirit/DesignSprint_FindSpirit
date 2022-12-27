249710c2e994dcda393f9a2f1c33e61649dafacd
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2022-12-27 16:54:08 +0900

    Update orderListIndex.dart

--- findspirit/lib/OrderList/orderListIndex.dart
+++ findspirit/lib/OrderList/orderListIndex.dart
@@ -10,11 +10,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading:
-              IconButton(icon: Icon(Icons.arrow_back_ios_new), onPressed: null),
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+              onPressed: null),
           actions: [
-            IconButton(icon: Icon(Icons.home), onPressed: null),
-            IconButton(icon: Icon(Icons.shopping_cart), onPressed: null),
+            IconButton(
+                icon: Icon(Icons.home, color: Colors.black), onPressed: null),
+            IconButton(
+                icon: Icon(Icons.shopping_cart, color: Colors.black),
+                onPressed: null),
           ],
           backgroundColor: Colors.white,
         ),
