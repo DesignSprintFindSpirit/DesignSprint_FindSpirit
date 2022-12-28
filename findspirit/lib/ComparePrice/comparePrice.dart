82a858df38a61258df38c177f0b2c253d88d1c59
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2022-12-28 15:50:40 +0900

    Update comparePrice.dart

--- findspirit/lib/ComparePrice/comparePrice.dart
+++ findspirit/lib/ComparePrice/comparePrice.dart
@@ -1,27 +1,129 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(ComparePrice());
+void main() {
+  runApp(MyApp());
+}
 
-class ComparePrice extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+            onPressed: null),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.home, color: Colors.black), onPressed: null),
+          IconButton(
+              icon: Icon(Icons.shopping_cart, color: Colors.black),
               onPressed: null),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.home, color: Colors.black), onPressed: null),
-            IconButton(
-                icon: Icon(Icons.shopping_cart, color: Colors.black),
-                onPressed: null),
+        ],
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(height: 200),
+            DefaultTabController(
+                length: 3, // length of tabs
+                initialIndex: 0,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Container(
+                        child: TabBar(
+                          indicatorColor: Color(0xFF7E5354),
+                          labelColor: Color(0xFF7E5354),
+                          unselectedLabelColor: Colors.black,
+                          tabs: [
+                            Tab(text: '주문'),
+                            Tab(text: '술 정보'),
+                            Tab(text: '리뷰'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                          height: 400, //height of TabBarView
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  top: BorderSide(
+                                      color: Colors.grey, width: 0.5))),
+                          child: TabBarView(children: <Widget>[
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 1',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 2',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                child: Text('Display Tab 3',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                          ]))
+                    ])),
           ],
-          backgroundColor: Colors.white,
         ),
-        body: ListView(
-          children: [],
+      ]),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(200, 50),
+                  side: const BorderSide(width: 1, color: Colors.black),
+                  backgroundColor: Colors.white),
+              onPressed: () {},
+              child: const Text('장바구니 담기',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.black)),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(200, 50),
+                  side: const BorderSide(width: 1, color: Colors.black),
+                  backgroundColor: Colors.black),
+              onPressed: () {},
+              child: const Text('주문하기',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.white)),
+            ),
+          ],
         ),
       ),
     );
