ab3ebcddf6f9c797b567d6fa1b495526d92831e3
YimKeul <leesjun29@gmail.com>
2022-12-28 11:20:56 +0900

    feat : edit style

--- findspirit/lib/OrderList/Widgets/qrCodeBox.dart
+++ findspirit/lib/OrderList/Widgets/qrCodeBox.dart
@@ -5,40 +5,37 @@ class qrCodeBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 13),
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 26),
-              child: Center(
-                  child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.qr_code,
-                    size: 200,
-                  ),
-                  Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
-                ],
-              )),
-            ),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
           ),
-          SizedBox(height: 16),
-        ],
-      ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 26),
+            child: Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.qr_code,
+                  size: 200,
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+              ],
+            )),
+          ),
+        ),
+        SizedBox(height: 16),
+      ],
     );
   }
 }
--- findspirit/lib/OrderList/orderListIndex.dart
+++ findspirit/lib/OrderList/orderListIndex.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './Widgets/qrCodeBox.dart';
 import './Widgets/DeliveryStatusBox.dart';
+import 'package:get/get.dart';
 
 void main() => runApp(OrderListIndex());
 
@@ -10,31 +11,63 @@ class OrderListIndex extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          iconTheme: IconThemeData(size: 40),
           leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
-              onPressed: null),
+            icon: Icon(Icons.arrow_back_ios_new),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
           actions: [
             IconButton(
-                icon: Icon(Icons.home, color: Colors.black), onPressed: null),
+              onPressed: () {
+                Get.offAllNamed('/');
+                // Get.offNamed(page)
+              },
+              icon: Icon(
+                Icons.home,
+                // size: 40,
+              ),
+            ),
             IconButton(
-                icon: Icon(Icons.shopping_cart, color: Colors.black),
-                onPressed: null),
+              onPressed: () {
+                Navigator.pushNamed(context, '/cart');
+              },
+              icon: Icon(
+                Icons.shopping_cart,
+                // size: 40,
+              ),
+            )
           ],
-          backgroundColor: Colors.white,
         ),
         body: ListView(
-          padding: EdgeInsets.all(13),
+          padding: EdgeInsets.symmetric(horizontal: 13),
           children: [
+            SizedBox(height: 32),
             Text("상품명 : 제임슨 스탠다드",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            SizedBox(height: 10),
             Text("픽업 장소 : 대전 유성구 궁동",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            SizedBox(height: 32),
             qrCodeBox(),
             Text(
               '배송 현황',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 33),
               textAlign: TextAlign.center,
             ),
+            SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 13),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.grey, width: 2))),
+              ),
+            ),
+            SizedBox(height: 10),
             deliveryStatusBox(),
             deliveryStatusBox(),
             deliveryStatusBox(),
