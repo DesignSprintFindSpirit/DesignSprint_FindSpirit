a84a41f1c39c862f66038284700bec4ee44447cf
glass2300 <51581244+glass2300@users.noreply.github.com>
2022-12-30 11:33:39 +0900

    update ad banner

--- findspirit/lib/Home/Widgets/AdBlock.dart
+++ findspirit/lib/Home/Widgets/AdBlock.dart
@@ -8,13 +8,23 @@ class adBlock extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 13),
       child: Column(
-        children: [
+        children: <Widget>[
+          // Image.asset('assets/banner/banner_hyungshin.png',
+          //     fit: BoxFit.contain),
           Container(
-            height: 170,
-            decoration: BoxDecoration(
-                color: Colors.amber[200],
-                borderRadius: BorderRadius.circular(12)),
-          ),
+              height: 170,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.amber[200],
+                  borderRadius: BorderRadius.circular(12)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  'assets/banner/banner_hyungshin.png',
+                  fit: BoxFit.fill,
+                ),
+              )),
+
           SizedBox(height: 16),
         ],
       ),
